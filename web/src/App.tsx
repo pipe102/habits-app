6628061cdd327329c75644ac905aaa71a867b2b8
Filipe Carvalho <luisfilipecarvalho10@gmail.com>
2023-02-09 21:50:54 +0000

    feat: frontend setup

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css'
+
+import Habit from "./components/Habit"
+
+function App() {
+
+  return (
+    <>
+      <Habit completed={3} />
+    </>
+  )
+}
+
+export default App
