22e232f09fc845e93ac51ba46e9aa8aee1eeab65
Filipe Carvalho <luisfilipecarvalho10@gmail.com>
2023-02-16 20:21:31 +0000

    feat:

---
+++ mobile/src/routes/app.routes.tsx
@@ -0,0 +1,18 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../screens/Home";
+import New from "../screens/New";
+import Habit from "../screens/Habit";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+const AppRoutes = () => {
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="home" component={Home} />
+      <Screen name="new" component={New} />
+      <Screen name="habit" component={Habit} />
+    </Navigator>
+  );
+};
+
+export default AppRoutes;
---
+++ mobile/src/screens/New.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import {
+  View,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import BackButton from "../components/BackButton";
+import Checkbox from "../components/Checkbox";
+import colors from "tailwindcss/colors";
+
+const availableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
+
+const New = () => {
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  const handleWeekDay = (weekDayIndex: number) => {
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays((prevState) =>
+        prevState.filter((weekDay) => weekDay !== weekDayIndex)
+      );
+    } else {
+      setWeekDays((prevState) => [...prevState, weekDayIndex]);
+    }
+  };
+
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
+        <Text className="mt-6 text-white font-extrabold text-3xl">
+          Create Habit
+        </Text>
+        <Text className="mt-6 text-white font-semibold text-base">
+          What's your commitment?
+        </Text>
+        <TextInput
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
+          placeholder="eg.: Workout, sleep well, etc.."
+          placeholderTextColor={colors.zinc[400]}
+        />
+        <Text className="font-semibold mt-4 mb-3 text-white text-base">
+          What's the recurrence?
+        </Text>
+        {availableWeekDays.map((weekDay, index) => (
+          <Checkbox
+            key={weekDay}
+            title={weekDay}
+            checked={weekDays.includes(index)}
+            onPress={() => handleWeekDay(index)}
+          />
+        ))}
+        <TouchableOpacity
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          activeOpacity={0.7}
+        >
+          <Feather name="check" size={20} color={colors.white} />
+          <Text className="font-semibold text-base text-white ml-2">
+            Confirm
+          </Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default New;
