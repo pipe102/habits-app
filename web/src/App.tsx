a4f45cfbb10b9bd7218e951f9e73b0f4fc4c2aca
Filipe Carvalho <luisfilipecarvalho10@gmail.com>
2023-02-17 12:25:00 +0000

    feat: connect the web with the server

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,19 @@
-import Header from './components/Header'
-import SummaryTable from './components/SummaryTable'
-import './styles/global.css'
-
+import Header from "./components/Header";
+import SummaryTable from "./components/SummaryTable";
+import "./styles/global.css";
+import "./lib/dayjs";
 
 //import Habit from "./components/Habit";
 
 function App() {
-
   return (
-    <div className='w-screen h-screen flex justify-center items-center'>
-      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
         <SummaryTable />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
