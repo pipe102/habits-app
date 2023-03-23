c64371e1a8e6824244387cfb2f16b53d9ef65d90
Filipe Carvalho <luisfilipecarvalho10@gmail.com>
2023-03-23 00:08:10 +0000

    feat: added the interface for the login and register

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,19 +4,28 @@ import "./styles/global.css";
 import "./lib/dayjs";
 import { useState } from "react";
 import Login from "./components/Login";
-
-//import Habit from "./components/Habit";
+import Register from "./components/Register";
 
 function App() {
   const [newHabitCreated, setNewHabitCreated] = useState(false);
+  const [wannaRegister, setWannaRegister] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-        <Header setNewHabitCreated={setNewHabitCreated} />
-        <SummaryTable newHabitCreated={newHabitCreated} />
-      </div>
-      {/* <Login /> */}
+      {isLoggedIn && (
+        <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+          <Header setNewHabitCreated={setNewHabitCreated} />
+          <SummaryTable newHabitCreated={newHabitCreated} />
+        </div>
+      )}
+      {wannaRegister && <Register setWannaRegister={setWannaRegister} />}
+      {!wannaRegister && !isLoggedIn && (
+        <Login
+          setWannaRegister={setWannaRegister}
+          setIsLoggedIn={setIsLoggedIn}
+        />
+      )}
     </div>
   );
 }
--- web/src/components/Register.tsx
+++ web/src/components/Register.tsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 
-interface LoginProps {
+interface RegisterProps {
   setWannaRegister: (value: boolean) => void;
-  setIsLoggedIn: (value: boolean) => void;
 }
 
-const Login = ({ setWannaRegister, setIsLoggedIn }: LoginProps) => {
+const Register = ({ setWannaRegister }: RegisterProps) => {
   const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log("Email:", email);
+    console.log("Name:", name);
     console.log("Password:", password);
   };
 
@@ -21,7 +22,7 @@ const Login = ({ setWannaRegister, setIsLoggedIn }: LoginProps) => {
       className="w-1/5 flex flex-col mt-6 bg-zinc-900 p-5 rounded-lg"
     >
       <div className="text-center">
-        <h1 className="text-4xl font-bold mb-10">Login</h1>
+        <h1 className="text-4xl font-bold mb-10">Register</h1>
       </div>
       <label htmlFor="email" className="font-semibold leading-tight">
         Email
@@ -35,6 +36,18 @@ const Login = ({ setWannaRegister, setIsLoggedIn }: LoginProps) => {
         value={email}
         onChange={(event) => setEmail(event.target.value)}
       />
+      <label htmlFor="email" className="font-semibold leading-tight">
+        Name
+      </label>
+      <input
+        type="text"
+        id="name"
+        placeholder="Jonh Doe"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
+        autoFocus
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
       <label htmlFor="password" className="font-semibold leading-tight">
         Password
       </label>
@@ -47,25 +60,23 @@ const Login = ({ setWannaRegister, setIsLoggedIn }: LoginProps) => {
         value={password}
         onChange={(event) => setPassword(event.target.value)}
       />
-      <div className="text-right mt-2">
-        <label className="leading-tight">Forgot password?</label>
-      </div>
 
       <button
         type="submit"
         className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
       >
-        Login
-      </button>
-      <button
-        type="submit"
-        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-violet-600 hover:bg-violet-500 transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
-        onClick={() => setWannaRegister(true)}
-      >
         Register
       </button>
+      <div className="text-center mt-2">
+        <label
+          className="leading-tight hover:cursor-pointer"
+          onClick={() => setWannaRegister(false)}
+        >
+          Wanna Login?
+        </label>
+      </div>
     </form>
   );
 };
 
-export default Login;
+export default Register;
